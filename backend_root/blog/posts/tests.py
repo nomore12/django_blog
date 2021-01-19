e54eac2ed0c4611bfc3a502af24776a0354ca773
Sungho <nightwing@gmail.com>
2021-01-19 18:05:41 +0900

    post model test success

--- backend_root/blog/posts/tests.py
+++ backend_root/blog/posts/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
 
-# Create your tests here.
+from posts.models import Post
+
+
+class PostsTestCase(TestCase):
+    def setUp(self):
+        self.post_01 = Post.objects.create(title="first post")
+        self.post_02 = Post.objects.create(title="second post")
+
+    def test_post_model_equals(self):
+        self.assertEqual("first post", self.post_01.title)
+        self.assertEqual("second post", self.post_02.title)
