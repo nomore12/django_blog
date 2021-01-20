420673ea47d0835a2c72f3498c39a93b2f46716b
Sungho <nightwing@gmail.com>
2021-01-20 17:46:54 +0900

    페이지 레이아웃 분리 시도 성공

--- backend_root/blog/blog/configs/base.py
+++ backend_root/blog/blog/configs/base.py
@@ -13,7 +13,7 @@ import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 
 # Quick-start development settings - unsuitable for production
@@ -110,3 +110,4 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = "/static/"
+STATIC_ROOT = f"{Path(BASE_DIR)}/static/"
--- backend_root/blog/posts/models.py
+++ backend_root/blog/posts/models.py
@@ -2,9 +2,13 @@ from django.db import models
 
 
 class Post(models.Model):
+    """
+    Post 모델
+    """
+
     title = models.CharField(max_length=100)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return f"{self.pk}"
+        return f"{self.title}"
