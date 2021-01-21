d7c04fd60dd592e2bde8b05ea2d25fb340a2c79e
Sungho <nightwing@gmail.com>
2021-01-21 14:28:00 +0900

    Post모델에 본문과 슬러그 필드 추가.

--- backend_root/blog/posts/models.py
+++ backend_root/blog/posts/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils.text import slugify
 
 
 class Post(models.Model):
@@ -6,9 +7,28 @@ class Post(models.Model):
     Post 모델
     """
 
-    title = models.CharField(max_length=100)
+    title = models.CharField(max_length=100, null=False)
+    slug = models.SlugField(
+        allow_unicode=True, max_length=100, default="default"
+    )
+    text = models.TextField(blank=False, default="default")
+    tag = models.ManyToManyField("Tag", blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+    def save(
+        self,
+        force_insert=False,
+        force_update=False,
+        using=None,
+        update_fields=False,
+    ):
+        self.slug = slugify(self.title, allow_unicode=True)
+        super().save(force_insert, force_update, using, update_fields)
+
     def __str__(self):
         return f"{self.title}"
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=40)
